5ae715935bd6ad0d8f3e787a50732bba783c470f
Jordan Conrad <Jordan.Conrad@hyland.com>
2020-02-26 19:08:46 -0500

    Week 3 code along

---
+++ Week 3 code along/Week 3 code along/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Week_3_code_along
+{
+    class Dog
+    {
+        public string Breed;
+        public int Height;
+        public string Name;
+
+        public void Bark()
+        {
+            MessageBox.Show("Bark");
+        }
+
+        public void SayName()
+        {
+            MessageBox.Show("My name is " + Name);
+        }
+    }
+}
