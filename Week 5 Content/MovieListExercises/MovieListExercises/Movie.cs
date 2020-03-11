5dd206847789f9afc387585c342880c714e6a159
Jordan Conrad <Jordan.Conrad@hyland.com>
2020-03-11 17:52:40 -0400

    Add MovieList Sln

---
+++ Week 5 Content/MovieListExercises/MovieListExercises/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace MovieList
+{
+    public class Movie
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public int ReleaseYear
+        {
+            get;
+            set;
+        }
+
+        public void ShowDetails()
+        {
+            string infoString = "Title: " + this.Title;
+            infoString += "\nRelease Year: " + this.ReleaseYear;
+            MessageBox.Show(infoString);
+        }
+
+        public Movie(string myTitle, int myReleaseYear)
+        {
+            this.Title = myTitle;
+            this.ReleaseYear = myReleaseYear;
+        }
+    }
+}
