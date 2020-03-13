a91a3c07015351001df4feccd01ebaf700bcf6e1
Joseph Maxwell <joseph.maxwell@hyland.com>
2020-03-13 10:54:56 -0400

    add inheritanceintro

---
+++ Week6/InheritanceIntro/InheritanceIntro/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InheritanceIntro
+{
+    public class Dog
+    {
+        public string Name;
+        public int Height;
+        public string Breed;
+
+        public void Bark()
+        {
+            MessageBox.Show("Bark");
+        }
+
+        public void SayName()
+        {
+            MessageBox.Show("My name is " + this.Name);
+        }
+
+        public Dog() { }
+
+        public Dog(string breed, int height, string name)
+        {
+            this.Breed = breed;
+            this.Height = height;
+            this.Name = name;
+        }
+    }
+}
